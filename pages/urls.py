da9961257094931e7d96453333325b9b2315bf33
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-01-30 18:04:15 +0545

    Working with frontend

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.home),
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('services/',views.services,name='services'),
+    path('about/',views.about,name='about'),
+    path('cars/',views.cars,name='cars'),
+    path('contacts/',views.contacts,name='contacts'),
+   
 ]
