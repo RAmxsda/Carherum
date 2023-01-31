1b95b8a1b1c6153a33cc4f41b4fc5058b3ec0af8
Ramesh <101050352+RAmxsda@users.noreply.github.com>
2023-01-31 12:15:48 +0545

    adding image from database to frontend

--- carzone/settings.py
+++ carzone/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST':'localhost',
     }
 }
 
@@ -124,6 +127,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'carzone/static')
 ]
 
+#Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
